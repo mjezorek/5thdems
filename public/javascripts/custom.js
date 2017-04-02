7dce9d52e2710b469ea18a581b649760f9c7c29d
Matt <mjezorek@gmail.com>
2017-04-02 12:37:35 -0700

    fixing http/https

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -51,11 +51,11 @@ function ordinal_suffix_of(i) {
 function getPrecinct() {
     $("#ldwarning").hide();
     var street = $('#address').val();
-    var url = "http://gismaps.kingcounty.gov/arcgis/rest/services/Address/Address_Points_locator/GeocodeServer/findAddressCandidates?Street=" + street + "&f=json&outSR=%7B%22wkid%22%3A102100%7D&outFields=Loc_name";
+    var url = "https://gismaps.kingcounty.gov/arcgis/rest/services/Address/Address_Points_locator/GeocodeServer/findAddressCandidates?Street=" + street + "&f=json&outSR=%7B%22wkid%22%3A102100%7D&outFields=Loc_name";
     $.getJSON(url, function(data) {
         var x = data['candidates'][0]['location']['x'];
         var y = data['candidates'][0]['location']['y'];
-        var dataURL = "http://gismaps.kingcounty.gov/arcgis/rest/services/Districts/KingCo_Electoral_Districts/MapServer/identify?f=json&geometry=%7B%22points%22%3A%5B%5B" + x + "%2C" + y + "%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&tolerance=3&returnGeometry=false&mapExtent=%7B%22xmin%22%3A-13668004.150429394%2C%22ymin%22%3A5927354.86630494%2C%22xmax%22%3A-13455814.959909849%2C%22ymax%22%3A6110497.986076133%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&imageDisplay=400%2C400%2C96&geometryType=esriGeometryMultipoint&sr=102100&layers=all%3A0%2C1%2C2%2C3%2C4"
+        var dataURL = "https://gismaps.kingcounty.gov/arcgis/rest/services/Districts/KingCo_Electoral_Districts/MapServer/identify?f=json&geometry=%7B%22points%22%3A%5B%5B" + x + "%2C" + y + "%5D%5D%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&tolerance=3&returnGeometry=false&mapExtent=%7B%22xmin%22%3A-13668004.150429394%2C%22ymin%22%3A5927354.86630494%2C%22xmax%22%3A-13455814.959909849%2C%22ymax%22%3A6110497.986076133%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&imageDisplay=400%2C400%2C96&geometryType=esriGeometryMultipoint&sr=102100&layers=all%3A0%2C1%2C2%2C3%2C4"
         $.getJSON(dataURL, function(data) {
             var p = data['results'][0]['value'];
             var ld = data['results'][3]['value'];
